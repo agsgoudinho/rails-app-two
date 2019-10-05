7d0aebef60668d75f14da1f5de100729fb740ba2
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-10-05 14:21:45 -0300

    add phones

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,6 +16,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    4.times { @contact.phones.build }
   end
 
   # GET /contacts/1/edit
@@ -75,7 +76,6 @@ class ContactsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def contact_params
-    params.require(:contact).permit(:name, :birthday)
-    # params.fetch(:contact, {})
+    params.require(:contact).permit(:name, :birthday, phones_attributes: [:number, :_destroy, :id])
   end
 end
