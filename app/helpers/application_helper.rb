eb4b0daebfb853b030f4bb4cb1b31425f5c39275
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-10-05 16:06:51 -0300

    show phones is working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def link_to_remove_fields(name, form)
+    form.hidden_field(:_destroy) +
+        link_to(name, "javascript:void(0)", onclick: "remove_fields(this)")
+  end
+
+  def link_to_add_fields(name, form, association)
+    new_object = form.object.class.reflect_on_association(association).klass.new
+    fields = form.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :form => builder)
+    end
+    link_to(name, "javascript:void(0)", onclick: h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+  end
 end
